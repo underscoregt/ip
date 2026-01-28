6c6bbb432e5c9073d621b3289f1e5e6f99bf4bf4
underscoregt <gabriel.jiaxu@gmail.com>
2026-01-28 18:05:40 +0800

    Create Amia.java
    Create chatbot

---
+++ src/main/java/Amia.java
@@ -0,0 +1,14 @@
+public class Amia {
+    public static void main(String[] args) {
+        start();
+        exit();
+    }
+
+    public static void start() {
+        System.out.println(" Hello! I'm Amia!\nWhat can I do for you?");
+    }
+
+    public static void exit() {
+        System.out.println("Bye!");
+    }
+}
