ae2577403eab8705b1a459da20a8e06950f9919c
underscoregt <gabriel.jiaxu@gmail.com>
2026-01-28 18:41:27 +0800

    Add tasks and update loop

--- src/main/java/Amia.java
+++ src/main/java/Amia.java
@@ -1,10 +1,54 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Amia {
+    private static ArrayList<String> tasks = new ArrayList<>();
+    private static int MAX_TASKS = 100;
+
     public static void main(String[] args) {
         start();
         loop();
         exit();
+    }
+
+    public static void loop() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String command = scanner.nextLine().trim();
+            switch (command.toLowerCase()) {
+                case "exit":
+                    scanner.close();
+                    return;
+                case "list":
+                    listTask();
+                    break;
+                default:
+                    addTask(command);
+            }
+        }
+    }
+
+    public static void addTask(String command) {
+        line();
+        if (tasks.size() < MAX_TASKS) {
+            System.out.println("\tAdded: " + command);
+            tasks.add(command);
+        } else {
+            System.out.println("\tTask list is full!");
+        }
+        line();
+    }
+
+    public static void listTask() {
+        line();
+        if (tasks.size() == 0) {
+            System.out.println("\tNo tasks to list!"); 
+            return;
+        }
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println("\t" + (i + 1) + ". " + tasks.get(i));
+        }
+        line();
     }
 
     public static void start() {
@@ -17,22 +61,6 @@ public class Amia {
         line();
         System.out.println("\tBye!");
         line();
-    }
-
-    public static void loop() {
-        Scanner scanner = new Scanner(System.in);
-        while (true) {
-            String command = scanner.nextLine().trim();
-            switch (command.toLowerCase()) {
-                case "exit":
-                    scanner.close();
-                    return;
-                default:
-                    line();
-                    System.out.println("\t" + command);
-                    line();
-            }
-        }
     }
 
     public static void line() {
