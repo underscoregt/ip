74960470b73ecb460612431586c017f4c6615153
underscoregt <gabriel.jiaxu@gmail.com>
2026-01-28 23:39:46 +0800

    Add task class, add mark/unmark class, rewrite printing

--- src/main/java/Amia.java
+++ src/main/java/Amia.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Amia {
-    private static ArrayList<String> tasks = new ArrayList<>();
+    private static ArrayList<Task> tasks = new ArrayList<>();
     private static int MAX_TASKS = 100;
 
     public static void main(String[] args) {
@@ -23,47 +23,93 @@ public class Amia {
                     listTask();
                     break;
                 default:
-                    addTask(command);
+                    if (command.startsWith("mark ")) {
+                        markTask(command);
+                    } else if (command.startsWith("unmark ")) {
+                        unmarkTask(command);
+                    } else {
+                        addTask(command);
+                    }
             }
         }
     }
 
     public static void addTask(String command) {
-        line();
+        say(line());
         if (tasks.size() < MAX_TASKS) {
-            System.out.println("\tAdded: " + command);
-            tasks.add(command);
+            say("Added: " + command);
+            tasks.add(new Task(command));
         } else {
-            System.out.println("\tTask list is full!");
+            say("Task list is full!");
         }
-        line();
+        say(line());
     }
 
     public static void listTask() {
-        line();
+        say(line());
         if (tasks.size() == 0) {
-            System.out.println("\tNo tasks to list!"); 
+            say("No tasks to list!"); 
+            say(line());
             return;
         }
         for (int i = 0; i < tasks.size(); i++) {
-            System.out.println("\t" + (i + 1) + ". " + tasks.get(i));
+            say((i + 1) + ". " + tasks.get(i));
         }
-        line();
+        say(line());
+    }
+
+    public static void markTask(String command) {
+        say(line());
+        try { 
+            int idx = Integer.parseInt(command.substring(5)) - 1;
+            if (idx >= 0 && idx < tasks.size()) {
+                tasks.get(idx).markDone();
+                say("I've marked the task as done!");
+                say("  " + tasks.get(idx));
+            } else {
+                say("Invalid task!");
+            }
+        } catch (NumberFormatException e) {
+            say(e.toString());
+        }
+        say(line());
+    }
+
+    public static void unmarkTask(String command) {
+        say(line());
+        try { 
+            int idx = Integer.parseInt(command.substring(7)) - 1;
+            if (idx >= 0 && idx < tasks.size()) {
+                tasks.get(idx).markUndone();
+                say("I've marked the task as not done yet.");
+                say("   " + tasks.get(idx));
+            } else {
+                say("Invalid task!");
+            }
+        } catch (NumberFormatException e) {
+            say(e.toString());
+        }
+        say(line());
     }
 
     public static void start() {
-        line();
-        System.out.println("\tHello! I'm Amia!\n\tWhat can I do for you?");
-        line();
+        say(line());
+        say("Hello! I'm Amia!");
+        say("What can I do for you?");
+        say(line());
     }
 
     public static void exit() {
-        line();
-        System.out.println("\tBye!");
-        line();
+        say(line());
+        say("\tBye!");
+        say(line());
     }
 
-    public static void line() {
-        System.out.println("\t-----------------------------------");
+    public static void say(String s) {
+        System.out.println("\t" + s);
+    }
+
+    public static String line() {
+        return "-----------------------------------";
     }
 }
