1cd1e9d4ebc94e557cc019f73a60ceb85a92f3e7
underscoregt <gabriel.jiaxu@gmail.com>
2026-02-10 00:26:16 +0800

    Use LocalDateTime for Deadline/Event dates
    Change Deadline and Event to store date/time as LocalDateTime and validate input.

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,14 +1,28 @@
-public class Deadline extends Task {
-    protected String by;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
-    public Deadline(String description, String by) {
+public class Deadline extends Task {
+    private static final DateTimeFormatter INPUT_FORMAT = 
+        DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm");
+    private static final DateTimeFormatter OUTPUT_FORMAT =
+        DateTimeFormatter.ofPattern("MMM dd yyyy, h:mma");
+    protected LocalDateTime by;
+    
+
+    public Deadline(String description, String byStr) throws AmiaException {
         super(description);
-        this.by = by;
+        try {
+            this.by = LocalDateTime.parse(byStr, INPUT_FORMAT);
+        } catch (DateTimeParseException e) {
+            throw new AmiaException("...Invalid date/time format. Use yyyy-MM-dd HHmm");
+        }
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: " + by.format(OUTPUT_FORMAT) + ")";
     }
 
     @Override
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,16 +1,29 @@
-public class Event extends Task {
-    protected String from;
-    protected String to;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
-    public Event(String description, String from, String to) {
+public class Event extends Task {
+    private LocalDateTime from;
+    private LocalDateTime to;
+
+    private static final DateTimeFormatter INPUT_FORMAT = 
+            DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm");
+    private static final DateTimeFormatter OUTPUT_FORMAT =
+            DateTimeFormatter.ofPattern("MMM dd yyyy, h:mma");
+
+    public Event(String description, String fromStr, String toStr) throws AmiaException{
         super(description);
-        this.from = from;
-        this.to = to;
+        try {
+            this.from = LocalDateTime.parse(fromStr, INPUT_FORMAT);
+            this.to = LocalDateTime.parse(toStr, INPUT_FORMAT);
+        } catch (DateTimeParseException e) {
+            throw new AmiaException("...Invalid date/time format. Use yyyy-MM-dd HHmm");
+        }
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
+        return "[E]" + super.toString() + " (from: " + from.format(OUTPUT_FORMAT) + " to: " + to.format(OUTPUT_FORMAT) + ")";
     }
 
     @Override
