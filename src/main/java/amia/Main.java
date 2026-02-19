07f3b52fd1fec90f7b4aee064463dd1353c5a30b
underscoregt <gabriel.jiaxu@gmail.com>
2026-02-19 23:18:54 +0800

    Set application window title to 'Amia'
    Gives the main stage a proper title.

--- src/main/java/amia/Main.java
+++ src/main/java/amia/Main.java
@@ -22,6 +22,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("Amia");
             fxmlLoader.<MainWindow>getController().setAmia(amia); // inject the Amia instance
             stage.show();
         } catch (IOException e) {
